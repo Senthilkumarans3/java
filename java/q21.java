f5641e9c5d2ae1496560f04efe0351a78aa68e66
Senthilkumarans3 <166887851+Senthilkumarans3@users.noreply.github.com>
2025-07-30 10:25:22 +0530

    Add files via upload

---
+++ java/q21.java
@@ -0,0 +1,12 @@
+class q21{
+    public static void main(String args[])
+    {
+        for(int i=1;i<=10;i++)
+        {
+         if(i%2==0)
+         {
+          System.out.println("Even number:"+i);
+         }
+        }
+    }
+}
---
+++ java/q23.java
@@ -0,0 +1,14 @@
+class q23{
+    public static void main(String args[])
+    {
+        int evencount = 0;
+        for(int i=1;i<=10;i++)
+        {
+            if(i%2==0)
+            {
+               evencount = evencount+1;
+            }
+        }
+        System.out.println(evencount);
+    }
+}
